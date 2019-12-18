3c109c50dcb8372ddb0b9db95d585ce74e7c5274
jroliveirati <jroliveirati@gmail.com>
2019-12-18 14:25:18 -0300

    Creating game field

--- App.js
+++ App.js
@@ -1,27 +1,48 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Text} from 'react-native'
 import params from './src/params'
-import Field from './src/components/Field'
-import Flag from './src/components/Flag'
+import MineField from './src/components/MineField'
+import { createMinedBoard } from './src/functions'
 
 export default class App extends Component {
+
+	constructor(props) {
+		super(props)
+		this.state = this.createState()
+	}
+
+	minesAmount = () => {
+		const cols = params.getColumnsAmount()
+		const rows = params.getRowsAmount()
+		return Math.ceil(cols * rows * params.difficultLevel)
+	}
+
+	createState = () => {
+		const cols = params.getColumnsAmount()
+		const rows = params.getRowsAmount()
+		return {
+			board: createMinedBoard(rows,cols,this.minesAmount())
+		}
+	}
+
 	render(){
 		return (
 			<View style={styles.container}>
-				<Field />
-				<Field opened />
-				<Field opened nearMines={6} />
-				<Field mined />
-				<Field mined opened />
-				<Field mined opened exploded />
-				<Field flagged />
-				<Field flagged opened />
-				<Flag bigger />
+				<View style={styles.board}>
+					<MineField board={this.state.board} />
+				</View>
 			</View>
 		)
 	}
 }
 
 const styles = StyleSheet.create({
-
+	container: {
+		flex:1,
+		justifyContent: 'flex-end',
+	},
+	board: {
+		alignItems: 'center',
+		backgroundColor: '#AAA'
+	}
 })
